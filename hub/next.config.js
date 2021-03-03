4fc5348aefac606cfed85ded7aa05c403457fa2f
ciaranmccullough <59839106+ciaranmccullough@users.noreply.github.com>
2021-03-03 20:38:43 +0000

    config files

--- hub/next.config.js
+++ hub/next.config.js
@@ -1,16 +1,16 @@
-// const TWENTYTWENTY_URL = 'https://vercel-test-2020.vercel.app';
-// const TWENTYTWENTYONE_URL = 'https://vercel-test-2021.vercel.app';
+const TWENTYTWENTY_URL = 'https://vercel-test-2020.vercel.app';
+const TWENTYTWENTYONE_URL = 'https://vercel-test-2021.vercel.app';
 
 module.exports = {
   rewrites() {
     return [
       {
         source: '/2020',
-        destination: `/2020`,
+        destination: `${TWENTYTWENTY_URL}/2020`,
       },
       {
         source: '/2021',
-        destination: `/2021`,
+        destination: `${TWENTYTWENTYONE_URL}/2021`,
       },
     ];
   },

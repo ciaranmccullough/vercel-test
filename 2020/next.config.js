b4b9d84c5b3edbfa57a55d1f1622a43b569e0d93
ciaranmccullough <59839106+ciaranmccullough@users.noreply.github.com>
2021-03-05 18:50:43 +0000

    config repo

--- 2020/next.config.js
+++ 2020/next.config.js
@@ -1,6 +1,3 @@
-const isProd = process.env.NODE_ENV === 'production';
-
 module.exports = {
-  assetPrefix: isProd ? 'https://cdn.mydomain.com' : '',
   basePath: '/2020',
 };
---
+++ hub/next.config.js
@@ -0,0 +1,17 @@
+const TWENTYTWENTY_URL = 'https://vercel-test-2020.vercel.app/';
+const TWENTYTWENTYONE_URL = 'https://vercel-test-2021.vercel.app/';
+
+module.exports = {
+  rewrites() {
+    return [
+      {
+        source: '/2020',
+        destination: `${TWENTYTWENTY_URL}/2020`,
+      },
+      {
+        source: '/2021',
+        destination: `${TWENTYTWENTYONE_URL}/2021`,
+      },
+    ];
+  },
+};

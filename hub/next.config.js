582df3a5f3ac4f60345511d3e9c523581f14aaf8
ciaranmccullough <59839106+ciaranmccullough@users.noreply.github.com>
2021-03-18 21:48:37 +0000

    update next

--- hub/next.config.js
+++ hub/next.config.js
@@ -2,15 +2,15 @@ const TWENTYTWENTY_URL = 'https://vercel-test-2020.vercel.app';
 const TWENTYTWENTYONE_URL = 'https://vercel-test-2021.vercel.app';
 
 module.exports = {
-  rewrites() {
+  async rewrites() {
     return [
       {
-        source: '/2020',
-        destination: `${TWENTYTWENTY_URL}/2020`,
+        source: '/2021/:path*',
+        destination: `${TWENTYTWENTY_URL}/2020/:path*`,
       },
       {
         source: '/2021',
-        destination: `${TWENTYTWENTYONE_URL}/2021`,
+        destination: `${TWENTYTWENTYONE_URL}/2021/:path*`,
       },
     ];
   },
